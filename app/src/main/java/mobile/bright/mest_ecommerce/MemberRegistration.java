7e1a89e002ca9a210343b9101f683711e809f3c6
Bright Ahedor <bright2kwame@gmail.com>
2023-01-17 23:43:25 +0000

    added login and splash screen

--- app/src/main/java/mobile/bright/mest_ecommerce/MemberRegistration.java
+++ app/src/main/java/mobile/bright/mest_ecommerce/MemberRegistration.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Objects;
+
 public class MemberRegistration extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_registration);
+
+
+        TextInputEditText textInputEditTextName = findViewById(R.id.editTextName);
+        String name = Objects.requireNonNull(textInputEditTextName.getText()).toString();
+        if (name.isEmpty()){
+
+        }
     }
 }
